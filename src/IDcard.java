2fd12294df73d5eba881969de6cf76e0df70a602
Sehdev <samarthsehdev502@gmail.com>
2025-07-14 11:02:10 +0530

    Initial commit

---
+++ src/IDcard.java
@@ -0,0 +1,7 @@
+public class IDcard {
+    public static void main(String[] args) {
+        System.out.println("Name: Samarth Sehdev");
+        System.out.println("Roll No. 2210990773");
+        System.out.println("Branch: CSE");
+    }
+}
